b2fbf4863e654bea075975ff534c22ed14451796
SumanDas05 <YOUR_GITHUB_EMAIL>
2026-07-29 19:29:56 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// This is a JavaScript comment. Anything after // on a line is ignored.
+
+// console.log() prints a message to the browser's "Console" — a developer tool
+// we use constantly to check that our code is running and to debug problems.
+console.log("script.js is connected and running!");
